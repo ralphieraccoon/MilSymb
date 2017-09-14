cd78dea6b49c879a9db2cb9feecc6e3704f27236
ralphieraccoon <djcrosby@f2s.com>
2017-09-14 16:52:08 +0100

    Fixed errors in some symbols.

--- manual_scripts/gen_symbol_tables.py
+++ manual_scripts/gen_symbol_tables.py
@@ -6,13 +6,14 @@ def maketable(x, y):
     l = list()
     s = re.split('}(?:\n|, *\n) *', x)
     del s[-1]
-    c = re.match('^(.*?)/', x).group(1)  # get type
+    c = re.match('^ *(.*?)/', x).group(1)  # get type
     for j in s:  # split up into each definition
         n = re.search('.*?/(.*?)/', j).group(1)  # get name
         if n == 'none':
             continue  # skip if a blank placeholder
-        ro = n.title() + r' & \trimbox{0cm, 0.25cm, 0.275cm, 0.25cm}{\tikz[baseline=-0.5ex]{\NATO' + y + \
-             r'[scale=2, faction=none, ' + c + r'=' + n + r']{(0,0)}}} \\ \hline'
+        ro = n.title() + r' & \trimbox{0.25cm, 0.25cm, 0.25cm, 0.25cm}' \
+                         r'{\tikz[baseline=-0.5ex, scale=2, transform shape]{\NATO' + y + \
+                         r'[faction=none, ' + c + r'=' + n + r']{(0,0)}}} \\ \hline'
         l.append(ro)
     ta = '\\begin{longtable}{|c|c|c|}\n\\hline\n\\bfseries{Name} & \\bfseries{Symbol} & \\bfseries{' \
          'Examples} \\\\ ''\n\\hline\n' + '\n'.join(l) + '\n\\end{longtable}'
@@ -31,8 +32,8 @@ for i in ['Air', 'Missile', 'Land']:
     if i != 'Missile':
         t = re.search(
             r'\\newcommand{\\NATO' + i + '\}\[\d\]\[\]{.*?main/\.is choice,\n *(.*?)^(?! *main)'
-                                         r'.*upper/\.is choice,\n *(.*?)^(?! *upper)'
-                                         r'.*lower/\.is choice,\n *(.*?)^(?! *lower)',
+                                         r'.*?upper/\.is choice,\n *(.*?)^(?! *upper)'
+                                         r'.*?lower/\.is choice,\n *(.*?)^(?! *lower)',
             r, re.S | re.M)  # extract all main, upper, lower
         maketable(t.group(1), i)  # main
         maketable(t.group(2), i)  # upper
@@ -40,8 +41,8 @@ for i in ['Air', 'Missile', 'Land']:
     else:
         t = re.search(
             r'\\newcommand{\\NATOMissile\}\[\d\]\[\]{'
-            r'.*left/\.is choice,\n *(.*?)^(?! *left)'
-            r'.*right/\.is choice,\n *(.*?)^(?! *right)',
+            r'.*left/\.is choice,\n *?(.*?)^(?! *left)'
+            r'.*right/\.is choice,\n *?(.*?)^(?! *right)',
             r, re.S | re.M)
         maketable(t.group(1), i)  # left
         maketable(t.group(2), i)  # right
