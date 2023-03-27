b296572aeb1bf28b276a0fb42aa50af61444adf4
Ralphie Raccoon <damian.crosby@manchester.ac.uk>
2023-03-27 21:04:43 +0100

    Fixed bug in release script.

--- release.ps1
+++ release.ps1
@@ -14,11 +14,11 @@ $lastmod = git log -1 --format=%cs
        -replace "\\vhEntry{$ver}{(.*?)}{(.*?}{.*?)}\n", "\vhEntry{$ver}{$lastmod}{$2}\n"
 }  | Set-Content milsymb.tex
 
-Copy-Item -Path "LICENCE.md","README.md","milsymb.sty","milsymb.pdf","milsymb.tex","manual_examples" -Destination (New-Item -Type Directory "milsymb") -Recurse -Force
+Copy-Item -Path "LICENCE.md","README.md","milsymb.sty","milsymb.pdf","milsymb.tex","manual_examples" -Destination (New-Item -Type Directory "milsymb") -Recurse
 Set-Location -Path "milsymb"
-ForEach-Object -InputObject "LICENCE.md","README.md","milsymb.sty","milsymb.tex","manual_examples/mef.tex" -Process {((Get-Content $_) -join "`n") + "`n" | Set-Content -NoNewline $_}
+"LICENCE.md","README.md","milsymb.sty","milsymb.tex","manual_examples/mef.tex" | ForEach-Object -Process {((Get-Content $_) -join "`n") + "`n" | Set-Content -NoNewline $_}
 Set-Location -Path "..\"
-Compress-Archive -Path "milsymb" -Destination "milsymb.zip" -Force 
+Compress-Archive -Path "milsymb" -Destination "milsymb.zip" -Update
 Remove-Item -Recurse ".\milsymb\"
 
 Set-Content release_${ver}_description.txt -Value "$ver $lastmod $desc"
